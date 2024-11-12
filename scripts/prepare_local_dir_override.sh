d41ecacc2c29c33c7d9ee10c4ec14500400eaddc
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-11-12 08:53:15 +0100

    [chore] Improve error handling and add debug message
    Add an error message for failed Bazel build to improve troubleshooting. Additionally, include an echo statement to display the target directory for the unpacked release.

--- scripts/prepare_local_dir_override.sh
+++ scripts/prepare_local_dir_override.sh
@@ -10,10 +10,12 @@ local_path_override(
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.." || exit 1
 
-bazel build //:rules_kotlin_release
+bazel build //:rules_kotlin_release || fail "cannot pack release"
 
 # unpack to for repository overriding
 ARCHIVE_DIR="bazel-bin/rules_kotlin_release"
+
+echo "Unpack to $ARCHIVE_DIR"
 rm -rf "$ARCHIVE_DIR"
 mkdir "$ARCHIVE_DIR"
-tar -C "$ARCHIVE_DIR" -xzvf bazel-bin/rules_kotlin_release.tgz
+tar -C "$ARCHIVE_DIR" -xzf bazel-bin/rules_kotlin_release.tgz
